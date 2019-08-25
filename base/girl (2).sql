fe70311b1107baf7ef0aa4c53b8e4eccb5efb1df
Pamela <panayelyaguilar@gmail.com>
2019-08-25 17:36:55 -0600

    Lo ultimo y base

--- base/girl (2).sql
+++ base/girl (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-07-2019 a las 07:09:26
+-- Tiempo de generación: 26-08-2019 a las 01:34:31
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 5.6.30
 
